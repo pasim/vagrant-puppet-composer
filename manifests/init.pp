f55742e24676df521243181e8850626d4a0ab3e6
pasim <janis.janovskis@gmail.com>
2014-06-18 10:41:31 +0100

    Resolving unmet dependency error
    We need to be sure that curl installed and we are not getting unmet dependency error

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,9 @@
 class composer {
+
+  package { "curl":
+    ensure => installed,
+  }
+
   exec { 'composer_install':
     command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer',
     path    => '/usr/bin:/usr/sbin',
